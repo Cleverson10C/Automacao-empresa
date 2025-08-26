2e3cf230d60bad4021fe4eabd5f0fea4a279cff5
Cleverson <clevercleverpassos@gmail.com>
2025-08-25 21:26:02 -0300

    Ajustes e melhorias no código de automação

--- posicoes.py
+++ posicoes.py
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 import pyautogui
-from time import sleep
+import time 
 
-sleep(5)
+time.sleep(5)
 print(pyautogui.position())
-
-=======
-import pyautogui
-from time import sleep
-
-sleep(5)
-print(pyautogui.position())
-
->>>>>>> a861d77399e34f035daa6c53bd750172d48eb702
-# pyautogui.scroll(1000)
